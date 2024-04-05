822c55e82c1abc5eba339f0f4d7e346fd0023118
Adarsh Gopalakrishnan <agkrishnan2003@gmail.com>
2024-04-05 12:12:10 +0530

    Uploaded resources

---
+++ Mechanical Design/Fusion to URDF/Simulation testing/ws1/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jk/ROS/ws1/src'
+whitelisted_packages = 'arm_to_goal'.split(';') if 'arm_to_goal' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jk/ROS/ws1/devel;/opt/ros/noetic'.split(';') if '/home/jk/ROS/ws1/devel;/opt/ros/noetic' != '' else []
